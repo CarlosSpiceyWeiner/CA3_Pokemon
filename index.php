16c7a050d4cad253cfb6ae19940adb785a839b67
Nathan Farrell Lynn <nathanjosephfl@gmail.com>
2019-03-01 12:02:51 +0000

    Version 1- Added my index page that links to the pokemon manager.

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php include 'view/header.php'; ?>
+<link href="main.css" rel="stylesheet" type="text/css"/>
+<main>
+    <h1>Menu</h1>
+    <ul>
+        <li>
+            <a href="pokemon_manager">pokemon Manager</a>
+        </li>
+    </ul>
+</main>
+<?php include 'view/footer.php'; ?>
