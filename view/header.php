40224793ec209b81f0fb9f7e5a41b4ca21a707d2
Nathan Farrell Lynn <nathanjosephfl@gmail.com>
2019-03-01 13:03:43 +0000

    Version 11- Added a new title that i got on a website called font meme.

--- view/header.php
+++ view/header.php
@@ -8,5 +8,5 @@
 
 <!-- the body section -->
 <body>
-<header><h1>Pokemon</h1></header>
+<header><h1><a href="https://fontmeme.com/pokemon-font/"><img src="https://fontmeme.com/permalink/190301/1dc68509b2f48aa3e81341f7644b656a.png" alt="pokemon-font" border="0"></a></a></h1></header>
        
